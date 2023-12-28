51faedd96a140aa486c936c16984d18037430860
joss <joss1091@gmail.com>
2023-12-27 19:29:07 -0500

    se agrega purge css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '../styles/libraries.css'
 
 import '../styles/slicky-carousel.css'
 import "../styles/scss/style.scss"
+// import "@fortawesome/fontawesome-free/scss/fontawesome.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,36 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  plugins: {
-    
-    autoprefixer: {},
-  }
-  
-}
+  plugins: [
+    "autoprefixer",,
+    // restore the Next.js default behavior
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+    [
+      // configure PurgeCSS
+      "@fullhuman/postcss-purgecss",
+      {
+        content: ['./pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: {
+          standard: ["html", "body"],
+          deep: [
+            /slick$/,
+          ],
+        },
+      },
+    ],
+  ],
+};
