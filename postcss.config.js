07bf57d2e734e6d8a7f6398319f2fbf94e54073c
joss <joss1091@gmail.com>
2024-02-03 11:49:30 -0500

    cambios

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ const purgecss  = [ [
       './pages/**/*.{js,jsx,ts,tsx}',
       './components/**/*.{js,jsx,ts,tsx}'],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    safelist: ["html", "body",/^d-/, /^slick-/, /^fa-/]
+    safelist: ["html", "body",/^d-/, /^slick-/]
     ,
   }],
 ]
